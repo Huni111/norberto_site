04e05142fc437caddaad71a05ca7b20e99df9100
Huni111 <huni19966@gmail.com>
2025-05-01 11:51:46 +0300

    contul meu

--- src/components/Cont.jsx
+++ src/components/Cont.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
+import '../styles/Cont.css';
 
-const Contul = () => {
-    return(
-        <>
-        <h1>Contul meu</h1>
-        </>
-    )
-}
+const Cont = () => {
+    return (
+        <div className="cont">
+            <h1>Informații Cont</h1>
+            <p>Nume Companie: SC Example SRL</p>
+            <p>CUI: RO12345678</p>
+            <p>Adresă: Str. Exemplu 123, București</p>
+            <p>Contact: 0712 345 678</p>
+            <p>Email: contact@example.com</p>
+        </div>
+    );
+};
 
-export default Contul;
+export default Cont;
